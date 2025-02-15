c134778fed4b29eecf837140e8bea7dc58150fc2
Luiz-ROCampos <ricardoxluiz88@gmail.com>
2025-02-15 18:24:46 -0300

    Commit: alunos, cadastro dos alunos

--- treino/api.py
+++ treino/api.py
@@ -1,7 +1,32 @@
 from ninja import Router
+from ninja.errors import HttpError
+from .schemas import AlunosSchema
+from .models import Alunos
+from typing import List
 
 treino_router = Router()
 
-@treino_router.get('teste/')
-def criar_aluno(request):
-    return {'Ola Mundo': 'Ola Mundo.'}
+@treino_router.post('/', response={200: AlunosSchema})
+def criar_aluno(request, aluno_schema: AlunosSchema):
+    nome = aluno_schema.dict()['nome']
+    email = aluno_schema.dict()['email']
+    faixa = aluno_schema.dict()['faixa']
+    data_nascimento = aluno_schema.dict()['data_nascimento']
+
+    if Alunos.objects.filter(email=email).exists() or Alunos.objects.filter(nome=nome):
+        raise HttpError(400, "E-mail já cadastrado.")
+
+    aluno = Alunos(
+        nome=nome, 
+        email=email, 
+        faixa=faixa, 
+        data_nascimento=data_nascimento
+    )
+    aluno.save()
+    
+    return aluno
+
+@treino_router.get('/alunos/', response=List[AlunosSchema])
+def listar_alunos(request):
+    alunos = Alunos.objects.all()
+    return alunos
--- treino/models.py
+++ treino/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+faixa_choices = (
+        ('B', 'Branca'),
+        ('A', 'Azul'),
+        ('R', 'Roxa'),
+        ('M', 'Marrom'),
+        ('P', 'Preta')
+    )
+
+class Alunos(models.Model):
+    
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+    faixa = models.CharField(max_length=1, choices=faixa_choices, default='B')
+
+    def __str__(self):
+        return self.nome
+    
